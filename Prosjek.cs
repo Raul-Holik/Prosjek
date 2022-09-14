ba1166567e08943b88b9c423d1b1f1a16af32754
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-09-14 09:29:44 +0200

    Add files via upload

---
+++ Prosjek.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProsjekOcjena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // varijable
+            int brojOcjena=0, ocjena;
+            double prosjek=0, zbrojOcjena = 0;
+
+            // zaglavlje aplikacije
+            Console.WriteLine("########################");
+            Console.WriteLine("#   Izračunavanje prosjeka   #");
+            Console.WriteLine("##############################");
+            Console.WriteLine("Za kraj unesi nulu. ");
+
+            do
+            {
+                brojOcjena = +1;
+
+
+                // unos ocjena
+                Console.WriteLine("Unesi ocjenu. ");
+            start: ocjena = Convert.ToInt32(Console.ReadLine());
+                if (ocjena > 1 && ocjena <= 5)
+                {
+                    // unos u zbroj
+                    zbrojOcjena += ocjena;
+                }
+
+                if (ocjena == 1)
+                {
+                    Console.WriteLine("Nisi prošao razred. ");
+                    break;
+                }
+
+                if (ocjena > 5 || ocjena < 1)
+                {
+                    Console.WriteLine("Pogrešan unos. Upišite ponovno. ");
+                    goto start;
+                }
+
+
+
+
+            } while (ocjena != 0);
+
+            prosjek = zbrojOcjena / brojOcjena;
+            Console.WriteLine(prosjek);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
